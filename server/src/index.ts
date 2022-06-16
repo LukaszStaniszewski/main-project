3bbe891899d1c0e19a07d307708aeb305dbb8ade
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2022-06-16 23:15:38 +0200

    basic setup for express, mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,27 @@
-console.log("hello")
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import cors from "cors"
+dotenv.config({debug: true});
+
+import logger from "./utils/logger"
+
+mongoose.connect(process.env.MONGO_URL_CLOUD as string).
+catch(error => logger.error(error));
+
+const db = mongoose.connection
+
+db.on('error', error => logger.error(error))
+db.once('open', () => logger.info('🔓 Database connected 🔓'))
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+   logger.info(`🚀 Server is running at http://localhost:${PORT} 🚀 `)
+})
