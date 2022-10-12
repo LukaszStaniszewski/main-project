8c7cb5bc53dd4b545d3747308b2c605025a6afe4
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2022-10-12 14:46:14 +0200

    add docker

--- client/src/pages/home-page/hompe-page.component.tsx
+++ client/src/pages/home-page/hompe-page.component.tsx
@@ -5,27 +5,27 @@ import LatestItemsOverview from "../../components/latest-items/latest-items-over
 import LargestCollectionsOverview from "../../components/largest-collections/largest-collections-overview.component";
 
 const HomePage = () => {
-   const ref = useRef<HTMLHeadingElement>(null);
+  const ref = useRef<HTMLHeadingElement>(null);
 
-   return (
-      <section className=" relative z-0 pb-4">
-         <HeaderExtension />
-         <main className="bg-secondary w-90vw m-auto screen-height p-4 rounded grid grid-cols-4">
-            <figure className="col-start-1 col-end-4 mt-6 mr-2 pr-2 border-r-2 ">
-               <h2 className="pb-4">Top collections</h2>
-               <div ref={ref}>
-                  <LargestCollectionsOverview />
-               </div>
-            </figure>
-            <aside className="col-start-4 col-end-5">
-               <h2 className="py-5">Latest Items</h2>
-               <div className="overflow-y-auto">
-                  <LatestItemsOverview />
-               </div>
-            </aside>
-         </main>
-      </section>
-   );
+  return (
+    <section className=" relative z-0 pb-4">
+      <HeaderExtension />
+      <main className="bg-secondary w-90vw m-auto screen-height p-4 rounded grid grid-cols-4">
+        <figure className="col-start-1 col-end-4 mt-6 mr-2 pr-2 border-r-2 ">
+          <h2 className="pb-4">Top collections</h2>
+          <div ref={ref}>
+            <LargestCollectionsOverview />
+          </div>
+        </figure>
+        <aside className="col-start-4 col-end-5">
+          <h2 className="py-5">Latest Items</h2>
+          <div className="overflow-y-auto">
+            <LatestItemsOverview />
+          </div>
+        </aside>
+      </main>
+    </section>
+  );
 };
 
 export default HomePage;
--- server/src/index.ts
+++ server/src/index.ts
@@ -11,8 +11,8 @@ import socket from "./utils/socket.utils";
 import createExpressServer from "./utils/server";
 
 mongoose
-   .connect(process.env.MONGO_URL_CLOUD as string, { socketTimeoutMS: 45000 })
-   .catch((error) => logger.error(error));
+  .connect(process.env.MONGO_URL_CLOUD as string, { socketTimeoutMS: 45000 })
+  .catch((error) => logger.error(error));
 
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
@@ -22,27 +22,27 @@ const app = createExpressServer();
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-   cors: {
-      origin: process.env.ORIGIN,
-      methods: ["GET", "POST", "PATCH", "DELETE"],
-      allowedHeaders: ["Content-type", "Authorization", "x-refresh"],
-      credentials: true,
-   },
+  cors: {
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-type", "Authorization", "x-refresh"],
+    credentials: true,
+  },
 });
 
 if (process.env.NODE_ENV === "production") {
-   app.use(express.static(path.join(__dirname, "../../client/build")));
+  app.use(express.static(path.join(__dirname, "../../client/build")));
 
-   app.get("*", (req, res) =>
-      res.sendFile(path.resolve(__dirname, "../../", "client", "build", "index.html"))
-   );
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../../", "client", "build", "index.html"))
+  );
 } else {
-   app.get("/", (req, res) => res.send("Please set to production"));
+  app.get("/", (req, res) => res.send("Please set to production"));
 }
 
 const PORT = process.env.PORT || 8000;
 
 httpServer.listen(PORT, async () => {
-   logger.info(`🚀 Server is running at http://localhost:${PORT} 🚀 `);
-   socket({ io });
+  logger.info(`🚀 Server is running at http://localhost:${PORT} 🚀 `);
+  socket({ io });
 });
